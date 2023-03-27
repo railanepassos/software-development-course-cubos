4825a958482c384ae103e639d587559557d76b5e
Railane Passos <railanepassos@gmail.com>
2023-03-27 09:11:29 -0300

    Exercicios extra aula de condicionais.

--- backend/back-noturno-condicionais/extra-12/index.js
+++ backend/back-noturno-condicionais/extra-12/index.js
@@ -1,4 +1,13 @@
 const primeiroNome = "Mario";
 const sobrenome = "";
-const apelido = "";
+const apelido = "Mai";
 
+if (apelido) {
+  console.log(apelido)
+}
+else if (sobrenome) {
+  console.log(`${primeiroNome} ${sobrenome}`)
+}
+else if (primeiroNome) {
+  console.log(primeiroNome)
+}
